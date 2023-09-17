8c4e11e6d33f0df83a18895653cc27cb167c26e8
LarissaAngioni <larissaangioni@gmail.com>
2023-09-17 11:53:22 -0300

    feat: Adicionar endpoint POST /login

--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { cadastrarUsuario } = require("./controladores/usuarioControlador");
+const {
+  cadastrarUsuario,
+  login,
+} = require("./controladores/usuarioControlador");
 
 const rotas = express();
 
 rotas.post("/usuario", cadastrarUsuario);
+rotas.post("/login", login);
 
 module.exports = rotas;
