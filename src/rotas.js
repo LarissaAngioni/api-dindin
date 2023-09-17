32a4e93c26ce7d0778d4a1994c6f822874e9e292
LarissaAngioni <larissaangioni@gmail.com>
2023-09-17 12:13:35 -0300

    feat: Adicionar intermediário de verificação de token

--- src/rotas.js
+++ src/rotas.js
@@ -3,10 +3,13 @@ const {
   cadastrarUsuario,
   login,
 } = require("./controladores/usuarioControlador");
+const checaToken = require("./intermediarios/checaToken");
 
 const rotas = express();
 
 rotas.post("/usuario", cadastrarUsuario);
 rotas.post("/login", login);
+
+rotas.use(checaToken);
 
 module.exports = rotas;
