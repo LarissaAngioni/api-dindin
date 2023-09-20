26ee05a52482c01c2b254905fed3220a905a0f1e
Milly <lauraa9lemos@gmail.com>
2023-09-19 21:09:18 -0300

    Feat: fiz as validações

--- src/controladores/trasacaoControlador.js
+++ src/controladores/trasacaoControlador.js
@@ -19,6 +19,61 @@ const listarTransaçaoes = async (req, res) => {
     }
 }
 
+const cadastrarTransacoes = async (req, res) => {
+    const usuario = req.usuario;
+    const { descricao, valor, data, categoria_id, tipo } = req.body;
+
+    try {
+        if (!descricao) {
+            return res.status(400).json({ mensagem: "O campo descrição é obrigatório" });
+          }
+        if (!valor) {
+            return res.status(400).json({ mensagem: "O campo valor é obrigatório" });
+          }
+        if (!data) {
+            return res.status(400).json({ mensagem: "O campo data é obrigatório" });
+          }
+        if (!categoria_id) {
+            return res.status(400).json({ mensagem: "O campo Categoria Id é obrigatório" });
+          }
+          if (!tipo) {
+            return res.status(400).json({ mensagem: "O campo tipo é obrigatório" });
+          }
+
+
+        if (valor <= 0) {
+            return res.status(400).json({ mensagem: "O campo Valor tem que ser maior do que zero " });
+          }
+
+          const categoriaExistente = await pool.query(
+            "select * from categorias where id = $1",
+            [categoria_id]
+          );
+      
+        if (categoriaExistente.rowCount <= 0) {
+            return res.status(404).json({
+              mensagem: "Esta Categoria Não Existe.",
+            });
+          }
+        
+          
+        
+        
+    } catch (error) {
+        
+    }
+}
+
+const obterExtratoTransacoes = async (req, res) => {
+    const usuario = req.usuario;
+
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
 module.exports = {
     listarTransaçaoes
 }
